b47696f54a611f2778678a0306c839a60c0a912a
duda <duda_ebbers@hotmail.com>
2023-11-08 12:42:36 -0300

    project lemonmart; estrutura basica e configuracoes router-first

---
+++ lemon-mart/src/app/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+const modules = [
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule
+];
+
+@NgModule({
+  imports: modules,
+  exports: modules
+})
+export class MaterialModule { }
