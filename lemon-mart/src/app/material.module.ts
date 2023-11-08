5de7f90bae944c4be57415b767bce0316202df25
duda <duda_ebbers@hotmail.com>
2023-11-08 17:09:04 -0300

    branding, color palette, favicon, custom themes, custom icons e material icons

--- lemon-mart/src/app/material.module.ts
+++ lemon-mart/src/app/material.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 
 const modules = [
   MatButtonModule,
