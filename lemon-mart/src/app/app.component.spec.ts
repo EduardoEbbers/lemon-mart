d31dabe82ec79bbc36b32344697fd70c296d6f21
duda <duda_ebbers@hotmail.com>
2023-11-08 19:50:36 -0300

    testing common

--- lemon-mart/src/app/app.component.spec.ts
+++ lemon-mart/src/app/app.component.spec.ts
@@ -1,11 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { DomSanitizerFake, MatIconRegistryFake, MediaObserverFake, commonTestingModules, commonTestingProviders } from './common/common.testing';
+import { MediaObserver } from '@angular/flex-layout';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    //imports: [RouterTestingModule],
+    imports: [commonTestingModules],
+    declarations: [AppComponent],
+    providers: commonTestingProviders.concat([
+      { provide: MediaObserver, useClass: MediaObserverFake },
+      { provide: MatIconRegistry, useClass: MatIconRegistryFake },
+      { provide: DomSanitizer, useClass: DomSanitizerFake },
+    ])
   }));
 
   it('should create the app', () => {
@@ -14,11 +24,13 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
+  /*
   it(`should have as title 'lemon-mart'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('lemon-mart');
   });
+  */
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
---
+++ lemon-mart/src/app/common/common.testing.ts
@@ -0,0 +1,72 @@
+import { SecurityContext } from "@angular/core";
+import { MediaChange } from "@angular/flex-layout";
+import { ReactiveFormsModule } from "@angular/forms";
+import { SafeResourceUrl, SafeValue } from "@angular/platform-browser";
+import { Observable, Subscription, of } from "rxjs";
+import { MaterialModule } from "../material.module";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { RouterTestingModule } from "@angular/router/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+
+const FAKE_SVGS = {
+    lemon: '<svg><path id="lemon" name="lemon"></path></svg>'
+};
+
+export class MediaObserverFake {
+    isActive(query: string): boolean {
+        return false;
+    }
+
+    asObservable(): Observable<MediaChange> {
+        return of({} as MediaChange);
+    }
+
+    subscribe(next?: (value: MediaChange) => void, error?: (error: any) => void, complete?: () => void): Subscription {
+        return new Subscription();
+    }
+}
+
+export class MatIconRegistryFake {
+    //tslint: disable-next-line: variable-name
+    _document = document;
+    addSvgIcon(iconName: string, url: SafeResourceUrl): this {
+        //this.addSvgIcon('lemon', 'lemon.svg')
+        return this;
+    }
+
+    getNameSvgIcon(name: string, namespace: string = ''): Observable<SVGElement> {
+        return of(this._svgNamedSvgIcon(FAKE_SVGS.lemon));
+    }
+
+    private _svgNamedSvgIcon(str: string): SVGElement {
+        const div = (this._document || document).createElement('div');
+        div.innerHTML = str;
+        const svg = div.querySelector('svg') as SVGElement
+        if(!svg) {
+            throw Error('<svg> tag not found');
+        }
+        return svg;
+    }
+}
+
+export class DomSanitizerFake {
+    bypassSecurityTrustResourceUrl(url: string): SafeResourceUrl {
+        return {} as SafeResourceUrl;
+    }
+
+    sanitize(context: SecurityContext, value: SafeValue | string | null): string | null {
+        return value?.toString() || null;
+    }
+}
+
+export const commonTestingProviders: any[] = [
+    //Intentionally Left Blank!!!
+];
+
+export const commonTestingModules: any[] = [
+    ReactiveFormsModule,
+    MaterialModule,
+    NoopAnimationsModule,
+    HttpClientTestingModule,
+    RouterTestingModule
+]
