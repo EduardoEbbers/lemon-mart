f927788fecda4c7739666e7d08c59b174d38183a
duda <duda_ebbers@hotmail.com>
2023-11-09 09:51:12 -0300

    funcionamento do logout

--- lemon-mart/src/app/user/logout/logout.component.ts
+++ lemon-mart/src/app/user/logout/logout.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
   styleUrls: ['./logout.component.css']
 })
-export class LogoutComponent {
+export class LogoutComponent implements OnInit {
+  constructor(private router: Router, private authService: AuthService) { }
 
+  ngOnInit() {
+    this.authService.logout(true);
+    this.router.navigate(['/']);
+  }
 }
